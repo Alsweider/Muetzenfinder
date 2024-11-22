fc2993db28815ba0eb69dd38b07fa05724fa618c
Alsweider <30653982+Alsweider@users.noreply.github.com>
2024-11-22 20:32:02 +0100

    v0.1.0

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -25,24 +25,66 @@ void MainWindow::on_pushButton_clicked()
 void MainWindow::on_plainTextEdit_textChanged()
 {
     QRegularExpression farbenRegex(
-        "(graubraun|dunkelgrau|hellgrün|blutrot|fleischfarbe|cremefarbe|"
+        "(graubraun|dunkelgrau|hellgrün|blutrot|fleischfarbe|cremefarbe[n]?|"
         "lila|türkis|rot|blau|grün|violett|pink|gelb|cyan|schwarz|"
         "(weiss|weiß)[nmr]?|grau|braun|ocker[n]?|orange[n]?|flieder[n]?)"
         "(e[nmr]?|em|es)?", QRegularExpression::CaseInsensitiveOption);
 
     QString text = ui->plainTextEdit->toPlainText();
-    QStringList gefundeneFarben;
+    QString htmlOutput = "<ol>";
 
     QRegularExpressionMatchIterator matchIterator = farbenRegex.globalMatch(text);
     while (matchIterator.hasNext()) {
         QRegularExpressionMatch match = matchIterator.next();
-        gefundeneFarben << match.captured(0);
+
+        QString colorName = match.captured(0);
+        QString squareColor = "black"; //Standardfarbe
+
+        // Farbe zuordnen
+        if (colorName.contains("rot", Qt::CaseInsensitive)) {
+            squareColor = "red";
+        } else if (colorName.contains("blau", Qt::CaseInsensitive)) {
+            squareColor = "blue";
+        } else if (colorName.startsWith("grün", Qt::CaseInsensitive)) {
+            squareColor = "green";
+        } else if (colorName.contains("gelb", Qt::CaseInsensitive)) {
+            squareColor = "yellow";
+        } else if (colorName.contains("violett", Qt::CaseInsensitive) || colorName.contains("lila", Qt::CaseInsensitive)) {
+            squareColor = "violet";
+        } else if (colorName.contains("pink", Qt::CaseInsensitive)) {
+            squareColor = "pink";
+        } else if (colorName.contains("schwarz", Qt::CaseInsensitive)) {
+            squareColor = "black";
+        } else if (colorName.contains("weiß", Qt::CaseInsensitive) || colorName.contains("weiss", Qt::CaseInsensitive)) {
+            squareColor = "white";
+        } else if (colorName.contains("grau", Qt::CaseInsensitive)) {
+            squareColor = "gray";
+        } else if (colorName.contains("orange", Qt::CaseInsensitive)) {
+            squareColor = "orange";
+        } else if (colorName.contains("türkis", Qt::CaseInsensitive)) {
+            squareColor = "turquoise";
+        } else if (colorName.contains("creme", Qt::CaseInsensitive)) {
+            squareColor = "#FFFDD0";
+        } else if (colorName.contains("ocker", Qt::CaseInsensitive)) {
+            squareColor = "#cc7722";
+        }  else if (colorName.contains("cyan", Qt::CaseInsensitive)) {
+            squareColor = "cyan";
+        } else if (colorName.contains("hellgrün", Qt::CaseInsensitive)) {
+            squareColor = "lime";
+        }
+
+        //HTML-Ausgabe mit farbigem Quadrat
+        htmlOutput += QString(
+                          "<li>"
+                          "<span style=\"color: %2; font-size: 1.2em;\">&#x25A0;</span> %1"
+                          "</li>")
+                          .arg(colorName, squareColor);
     }
 
-    QString filteredText = gefundeneFarben.join("\n");
+    htmlOutput += "</ol>";
+    qDebug() << "Generated HTML:\n" << htmlOutput;
 
-    ui->plainTextEdit_2->setPlainText(filteredText);
-
+     ui->plainTextEdit_2->setHtml(htmlOutput);
 }
 
 void MainWindow::on_pushButton_2_clicked()
